b9f4e53d8efb803d049507bc4733aedb42674601
easdale2010 <easdale2010@hotmail.co.uk>
2016-09-12 14:31:52 +0100

    Composite Pattern

---
+++ src/Waitress.java
@@ -0,0 +1,14 @@
+
+import java.util.Iterator;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
